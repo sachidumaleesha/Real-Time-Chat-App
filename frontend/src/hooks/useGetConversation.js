bd0beeda08cc35454b538c7874cdd7a537b08015
sachidumaleesha <diwansachidu2000@gmail.com>
2024-02-28 02:08:16 +0530

    fetch users to the sidebar
    implement zustand

---
+++ frontend/src/hooks/useGetConversation.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import {toast} from "react-hot-toast"
+
+const useGetConversation = () => {
+  const [loading, setLoading] = useState(false);
+  const [conversations, setConversation] = useState([]);
+
+  useEffect(() => {
+    const getConversations = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch("/api/users");
+        const data = await res.json();
+
+        if (data.error) {
+          throw new Error(data.error);
+        }
+        setConversation(data);
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getConversations();
+  }, []);
+
+  return { loading, conversations };
+};
+
+export default useGetConversation;
