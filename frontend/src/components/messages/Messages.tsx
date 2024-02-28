ed47172e464f78b4c6dd7e5c2452910c3ee1bcbc
sachidumaleesha <diwansachidu2000@gmail.com>
2024-02-28 17:14:48 +0530

    implement message function

--- frontend/src/components/messages/Messages.tsx
+++ frontend/src/components/messages/Messages.tsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import Message from './Message'
+import React from "react";
+import Message from "./Message";
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 
-const Messages = () => {
-  return (
-    <div className='px-4 flex-1 overflow-auto'>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-    </div>
-  )
+interface IMessage {
+  _id: string; // Assuming _id is a string in the frontend code
+  senderId: string; // Assuming senderId is a string in the frontend code
+  receiverId: string; // Assuming receiverId is a string in the frontend code
+  message: string;
+  createdAt: string; // Assuming createdAt is a string in the frontend code
+  updatedAt: string; // Assuming updatedAt is a string in the frontend code
 }
 
-export default Messages
+const Messages = () => {
+  const { loading, messages } = useGetMessages();
+  console.log(messages);
+  return (
+    <div className="px-4 flex-1 overflow-auto">
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message:IMessage) => (
+          <Message key={message._id} message={message} />
+        ))}
+
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Send a message to start the conversation</p>
+      )}
+    </div>
+  );
+};
+
+export default Messages;
---
+++ frontend/src/hooks/useGetMessages.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import useConversation from "../zustand/useConversation";
+
+const useGetMessages = () => {
+  const [loading, setLoading] = useState(false);
+  const { selectedConversation, messages, setMessages } = useConversation();
+
+  useEffect(() => {
+    const getMessages = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(`/api/messages/${selectedConversation._id}`);
+        const data = await res.json();
+
+        if (data.error) {
+          throw new Error(data.error);
+        }
+
+        setMessages(data);
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (selectedConversation?.id) getMessages();
+  }, [selectedConversation?.id, setMessages]);
+
+  return { loading, messages };
+};
+
+export default useGetMessages;
